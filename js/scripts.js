574ae76f184533ab44c43d3a702958f3ba39046a
thepsalmist <xf.xavierfrank@gmail.com>
2019-05-12 08:06:06 +0300

    create two variable to hold player details

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,11 @@
+var player1;
+var player2;
 $(document).ready(function() {
   $("#rulesButton").click(function() {
     $("#rules-of-game").show();
   });
+  $("#submitForm").submit(function(event) {
+    event.preventDefault();
+
+  })
 });
