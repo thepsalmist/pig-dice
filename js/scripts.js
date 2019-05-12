0acf7113a4da4b417d0f3ba7dd6985f03c30367d
thepsalmist <xf.xavierfrank@gmail.com>
2019-05-12 10:46:57 +0300

    grab player names from html form and display on player area

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,29 @@
 var player1;
 var player2;
+
+function Player(name, totalPoints, diceRoll, score) {
+  this.name = name;
+  this.totalPoints = 0;
+  this.diceRoll = 0;
+  this.score = 0;
+}
+
 $(document).ready(function() {
   $("#rulesButton").click(function() {
     $("#rules-of-game").show();
   });
-  $("#submitForm").submit(function(event) {
+  $("#submitForm").click(function(event) {
     event.preventDefault();
+    var firstPlayer = $("#namePlayer1").val();
+    var secondPlayer = $("#namePlayer2").val();
+    player1 = new Player(firstPlayer);
+    player2 = new Player(secondPlayer);
+    console.log("works")
+
+    $("#player1Name").text(player1.name);
+    $("#player2Name").text(player2.name);
+    console.log("no-work")
+
 
   })
 });
