62296ce6700e6071a921140cc85ffc0e877a2760
thepsalmist <xf.xavierfrank@gmail.com>
2019-05-13 10:48:14 +0300

    modify the reset functionality

--- js/scripts.js
+++ js/scripts.js
@@ -9,17 +9,6 @@ function Player(name, totalPoints, diceRoll, score) {
   this.score = 0;
 }
 
-function reset() {
-  $("#namePlayer1").val("");
-  $("#namePlayer2").val("");
-  var players = [player1, player2];
-  players.forEach(function(player) {
-    player.totalPoints = 0;
-    player.diceRoll = 0;
-    player.score = 0;
-  });
-
-}
 Player.prototype.roll = function() {
   var randomNumber = Math.floor(Math.random() * 6) + 1;
   this.diceRoll = randomNumber;
